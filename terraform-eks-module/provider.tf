f4ec4d2145b0e082801fa933a5eb5fb571804176
rafael <rafaelviniciuscardoso@gmail.com>
2022-04-08 19:03:19 -0300

    update role

--- terraform-eks-module/provider.tf
+++ terraform-eks-module/provider.tf
@@ -5,7 +5,6 @@ provider "aws" {
       environment   = var.environment
       owner         = var.owner
       provisionedBy = var.provisioner
-      createdAt     = formatdate("YYYY-MM-DD", timestamp())
     }
   }
 }
