d27c459f02693bade75f06d4780fe1da08f087e4
rafael <rafaelviniciuscardoso@gmail.com>
2022-04-08 17:16:07 -0300

    update module

--- terraform-vpc-module/provider.tf
+++ terraform-vpc-module/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
       environment   = var.environment
       owner         = var.owner
       provisionedBy = var.provisioner
-      #createdAt     = formatdate("YYYY-MM-DD", timestamp())
+      createdAt     = formatdate("YYYY-MM-DD", timestamp())
       #terraform-base-path = replace(path.cwd, "/^.*?(${local.terraform-git-repo}\\/)/", "$1")
     }
   }
